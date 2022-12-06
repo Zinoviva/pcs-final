b5bb11cba19b694bcd3844cb2bf457a204c5bae5
Yulia Zinovieva <Kiss-ka1-833@mail.ru>
2022-12-06 18:41:05 +0300

    'BooleanSearchEngine correct'

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,6 +1,8 @@
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
-import java.io.*;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -8,16 +10,17 @@ import java.util.stream.Collectors;
 public class BooleanSearchEngine implements SearchEngine {
     protected HashMap<String, ArrayList<PageEntry>> idxMap;
 
-    private void addToIdxMap(HashMap<String, ArrayList<PageEntry>> map, String[] words, String fn, int page){
-        for (String word: words) {
+    private void addToIdxMap(HashMap<String, ArrayList<PageEntry>> map, String[] words, String fn, int page) {
+
+        for (String word : words) {
             String lowerWord = word.toLowerCase();
             map.computeIfAbsent(lowerWord, k -> new ArrayList<>());
-            ArrayList <PageEntry> array = map.get(lowerWord);
-            List <PageEntry> filteredArray = array.stream().filter(pg -> pg.getPdfName().equals(fn) && pg.getPage() == page).collect(Collectors.toList());
-                if (!filteredArray.isEmpty()){
+            ArrayList<PageEntry> array = map.get(lowerWord);
+            List<PageEntry> filteredArray = array.stream().filter(pg -> pg.getPdfName().equals(fn) && pg.getPage() == page).collect(Collectors.toList());
+            if (!filteredArray.isEmpty()) {
                 PageEntry curPe = filteredArray.get(0);
                 curPe.setCount(curPe.getCount() + 1);
-            }else{
+            } else {
                 var newPe = new PageEntry(fn, page);
                 newPe.setCount(1);
                 array.add(newPe);
@@ -28,19 +31,12 @@ public class BooleanSearchEngine implements SearchEngine {
 
     @Override
     public List<PageEntry> search(String word) {
-        ArrayList <PageEntry> array = this.idxMap.get(word.toLowerCase());
-        if (array == null || array.isEmpty()){
+        ArrayList<PageEntry> array = this.idxMap.get(word.toLowerCase());
+        if (array == null || array.isEmpty()) {
             return Collections.emptyList();
-        }
-        else{
-            sortArray(array);
+        } else {
             return array;
         }
-    }
-
-    public  void sortArray (ArrayList<PageEntry> array){
-        Collections.sort(array);
-        Collections.reverse(array);
     }
 
     public BooleanSearchEngine(File directory) throws IOException {
@@ -48,21 +44,29 @@ public class BooleanSearchEngine implements SearchEngine {
         // тк во время поиска сервер не должен уже читать файлы
         idxMap = new HashMap<>();
         //загрузка документа
-        if (directory.isDirectory()){
+        if (directory.isDirectory()) {
             for (File f : Objects.requireNonNull(directory.listFiles())) {
-                try(PDDocument pdd = PDDocument.load(f)){
+                try (PDDocument pdd = PDDocument.load(f)) {
                     pdd.setAllSecurityToBeRemoved(true);
                     PDFTextStripper ts = new PDFTextStripper();
                     int count = pdd.getNumberOfPages();
-                    for (int i = 1; i < count ; i++) {
+                    for (int i = 1; i < count; i++) {
                         ts.setStartPage(i);
                         ts.setEndPage(i);
                         String text = ts.getText(pdd);
-                        String [] words = text.split("\\P{IsAlphabetic}+");
+                        String[] words = text.split("\\P{IsAlphabetic}+");
                         addToIdxMap(this.idxMap, words, f.getName(), i);
+                        sortListsInMap();
                     }
                 }
             }
+        }
+    }
+
+    private void sortListsInMap() {
+        for (Map.Entry<String, ArrayList<PageEntry>> entry : idxMap.entrySet()) {
+            Collections.sort(entry.getValue());
+            Collections.reverse(entry.getValue());
         }
     }
 }
